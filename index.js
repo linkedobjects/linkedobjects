8468d924c61f03e56d649eededa7db36e4cfb54e
Melvin Carvalho <melvincarvalho@gmail.com>
2023-04-22 17:19:28 +0200

    traverse both objects and arrays

--- index.js
+++ index.js
@@ -1,31 +1,30 @@
-// linkedObjects.js
-function _findNestedObjectById (objects, id) {
+function _findNestedObjectById(objects, id) {
   if (objects[id]) {
-    return objects[id]
+    return objects[id];
   }
 
   for (const key in objects) {
-    if (typeof objects[key] === 'object') {
-      const result = _findNestedObjectById(objects[key], id)
+    if (typeof objects[key] === 'object' || Array.isArray(objects[key])) {
+      const result = _findNestedObjectById(objects[key], id);
       if (result) {
-        return result
+        return result;
       }
     }
   }
 
-  return null
+  return null;
 }
 
-export function create (objects, id, data) {
+export function create(objects, id, data) {
   objects[id] = { ...data }
   return objects[id]
 }
 
-export function fetch (objects, id) {
+export function fetch(objects, id) {
   return _findNestedObjectById(objects, id) || null
 }
 
-export function update (objects, id, data) {
+export function update(objects, id, data) {
   const targetObject = _findNestedObjectById(objects, id)
 
   if (!targetObject) {
@@ -36,7 +35,7 @@ export function update (objects, id, data) {
   return targetObject
 }
 
-export function deleteObject (objects, id) {
+export function deleteObject(objects, id) {
   if (objects[id]) {
     delete objects[id]
     return true
@@ -54,7 +53,7 @@ export function deleteObject (objects, id) {
   return false
 }
 
-export function importJSON (objects, json) {
+export function importJSON(objects, json) {
   const inputData = JSON.parse(json)
 
   inputData.forEach((item) => {
@@ -63,7 +62,7 @@ export function importJSON (objects, json) {
   })
 }
 
-export function exportJSON (objects) {
+export function exportJSON(objects) {
   const outputData = Object.keys(objects).map((id) => {
     return { '@id': id, ...objects[id] }
   })
